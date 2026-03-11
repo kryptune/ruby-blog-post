0c82152a91bcba19bbb6977dd3b7ce4c5a024d4e
kryptune <rolandodelatorre13@gmail.com>
2026-03-11 11:18:48 +0800

    Add tests for blog post validation, scope and model methods

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,9 +1,20 @@
 class BlogPost < ApplicationRecord
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, :body, presence: true
 
   scope :draft, -> {where(published_at: nil)}
   scope :published, -> {where("published_at <= ?", Time.current )}
   scope :scheduled, -> {where("published_at > ?", Time.current )}
+
+  def draft?
+    published_at.nil?
+  end
+  def published?
+    published_at.present? && published_at <= Time.current 
+  end
+
+  def scheduled?
+    published_at.present? && published_at > Time.current 
+  end
+
 end
 
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,75 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  ### MODEL METHODS ###
+  test "draft? returns true for draft blog post" do
+     assert new_draft.draft?
+   end
+
+  test "draft? returns false for published blog post" do
+     refute new_published.draft?
+   end
+
+  test "draft? returns false for scheduled blog post" do
+     refute new_scheduled.draft?
+   end
+
+  test "published? returns true for published blog post" do
+     assert new_published.published?
+   end
+
+  test "published? returns false for draft blog post" do
+     refute new_draft.published?
+   end
+
+  test "published? returns false for scheduled blog post" do
+     refute new_scheduled.published?
+   end
+
+  ### VALIDATIONS ###
+  test "should not save when title is empty" do 
+    post = BlogPost.new(body: "Hello")
+    assert_not post.valid?
+    assert_includes post.errors[:title], "can't be blank"
+    post.errors.full_messages
+  end
+
+  test "should not save when body is empty" do 
+    post = BlogPost.new(title: "World")
+    assert_not post.valid?
+    assert_includes post.errors[:body], "can't be blank"
+    post.errors.full_messages
+  end
+
+  ### SCOPE ###
+  test "draft only contains draft blog posts" do
+    assert_includes BlogPost.draft, blog_posts(:draft)
+    assert_not_includes BlogPost.draft, blog_posts(:published)
+    assert_not_includes BlogPost.draft, blog_posts(:scheduled)
+   end
+
+  test "scheduled only contains scheduled blog posts" do
+    assert_includes BlogPost.scheduled, blog_posts(:scheduled)
+    assert_not_includes BlogPost.scheduled, blog_posts(:published)
+    assert_not_includes BlogPost.scheduled, blog_posts(:draft)
+   end
+
+  test "published only contains published blog posts" do
+    assert_includes BlogPost.published, blog_posts(:published)
+    assert_not_includes BlogPost.published, blog_posts(:draft)
+    assert_not_includes BlogPost.published, blog_posts(:scheduled)
+   end
+end
+
+def new_draft
+  BlogPost.new(published_at: nil)
+end
+
+def new_published
+  BlogPost.new(published_at: 1.year.ago)
+end
+
+def new_scheduled
+  BlogPost.new(published_at: 1.year.from_now)
 end
