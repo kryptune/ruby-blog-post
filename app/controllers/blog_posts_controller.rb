1080ab8c88c0c96e4c27291b864ff0729863a4cc
kryptune <rolandodelatorre13@gmail.com>
2026-03-16 09:56:08 +0800

    Add status column in db table and change index method to facilitate status selector

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,10 +4,18 @@ class BlogPostsController < ApplicationController
   before_action :set_blog_post, except: [ :index, :new, :create ]
 
   def index
-    @blog_posts = (user_signed_in? ? BlogPost.all : BlogPost.published).order(updated_at: :desc )
+    @blog_posts_all = BlogPost.all.order(updated_at: :desc)
+    @blog_posts = user_signed_in? ? BlogPost.all.order(updated_at: :desc) : BlogPost.published.order(updated_at: :desc)
+
+    if params[:status].present?
+      @blog_posts = @blog_posts.public_send(params[:status])
+    end
+
+    @blog_posts = @blog_posts.order(created_at: :desc)
   end
 
   def show;  end
+
 
   def new
     @blog_post = BlogPost.new
@@ -15,6 +23,9 @@ class BlogPostsController < ApplicationController
 
   def create
     @blog_post = BlogPost.new(blog_post_params)
+
+    set_blog_post_status
+
     if @blog_post.save
       redirect_to @blog_post, notice: "Blog post was successfully created."
     else
@@ -25,7 +36,11 @@ class BlogPostsController < ApplicationController
   def edit; end
 
   def update
+
     if @blog_post.update(blog_post_params)
+      set_blog_post_status
+      @blog_post.save
+
       redirect_to @blog_post, notice: "Blog post was successfully updated."
     else
       render :edit, status: :unprocessable_entity
@@ -52,6 +67,18 @@ class BlogPostsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: "Blog post not found."
   end
+
+  def set_blog_post_status
+    if @blog_post.published_at == nil
+      @blog_post.status.draft!
+    elsif  @blog_post.published_at > Time.current
+      @blog_post.status.scheduled!
+    else
+      @blog_post.status.published!
+    end
+  end
+
+
 
   def  authenticate_user
     unless user_signed_in?
