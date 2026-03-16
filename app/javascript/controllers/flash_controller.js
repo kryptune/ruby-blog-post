31b494acf4fb8ad1633534457801b016ab3a2181
kryptune <rolandodelatorre13@gmail.com>
2026-03-16 09:45:13 +0800

    Add stimulus for auto dismissing notifications after 3secs

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.element.classList.add("opacity-0");
+
+      setTimeout(() => {
+        this.element.remove();
+      }, 500);
+    }, 3000);
+  }
+}
