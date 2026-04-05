160f0112430f8b111b3fe1b7bce0c3c42d8bf1aa
kryptune <rolandodelatorre13@gmail.com>
2026-04-05 11:11:54 +0800

    Fix the issue of image not rendering when changed or removed

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -5,22 +5,12 @@ class BlogPost < ApplicationRecord
   has_one_attached :cover_image
   has_many_attached :images
 
-
-  after_update_commit :broadcast_status_change, if: :saved_change_to_status?
   after_update_commit :broadcast_blog_post_change
   after_create_commit :broadcast_new_blog_post
   after_destroy_commit :broadcast_delete_post
 
 
   private
-
-  def broadcast_status_change
-    Rails.logger.info "Broadcasting status change for blog post #{id} to #{status}"
-    # Update the status of blog post
-      broadcast_update_to "blog_posts", target: "blog_post_status", partial: "blog_posts/status", locals: { blog_post: self }
-    # Update the counter for the published, scheduled and draft
-      broadcast_update_to "blog_posts", target: "blog_posts_counter", partial: "blog_posts/counter" , locals: { blog_posts: BlogPost.all, logged_in: true }
-  end
 
   def broadcast_new_blog_post
     # Prepend new post to the grid
@@ -45,8 +35,27 @@ class BlogPost < ApplicationRecord
   end
 
   def broadcast_blog_post_change
-    # Update blogpost for changes
-      broadcast_replace_to "blog_posts", target: "blog_post_#{id}", partial: "blog_posts/blog_post", locals: { blog_post: self }
+    blog_post = self.reload
+    if (img = images.first) && img.variable?
+      img.variant(resize_to_fill: [300, 180]).processed rescue nil
+    end
+
+    broadcast_replace_to "blog_posts",
+      target: "blog_post_#{id}",
+      partial: "blog_posts/blog_post",
+      locals: { blog_post: blog_post }
+
+    # Only broadcast counter/status if they actually changed
+    if saved_change_to_status?
+      broadcast_update_to "blog_posts",
+        target: "blog_post_status_#{id}",
+        partial: "blog_posts/status",
+        locals: { blog_post: blog_post }
+      broadcast_update_to "blog_posts",
+        target: "blog_posts_counter",
+        partial: "blog_posts/counter",
+        locals: { blog_posts: BlogPost.all, logged_in: true }
+    end
   end
 
 end
