86f7a599d510dc67ed2bb30f1f3a64c49997a9d7
kryptune <rolandodelatorre13@gmail.com>
2026-04-05 11:17:57 +0800

    Add feature where user can only edit and delete their comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,7 +20,7 @@ class Comment < ApplicationRecord
     broadcast_prepend_to [blog_post, :comments],
       target: "comments",
       partial: "comments/comment",
-      locals: {comment: self , logged_in: true}
+      locals: {comment: self , logged_in: true, current_user: current_user}
   end
 
   def broadcast_delete_comment
@@ -33,7 +33,7 @@ class Comment < ApplicationRecord
     broadcast_replace_to [blog_post, :comments], 
       target: "comment_#{id}",
       partial: "comments/comment",
-      locals: {comment: self , logged_in: true}
+      locals: {comment: self , logged_in: true, current_user: current_user }
   end
 
 end
